308bdc14f974773584e1ee30298426dd3f4ca93d
MarikMMM <marik.mueller@stud.uni-heidelberg.de>
2024-05-03 13:20:51 +0200

    python file for functions

---
+++ function.py
@@ -0,0 +1,7 @@
+#function that calculates the relative amino acid composition based on sequence entry and definded amino acid property
+def rel_aa_comp(Sequence:str, AA_property:str) -> list: 
+    count = 0
+    for element in Sequence:  
+        if element in AA_property:
+            count += 1
+    return count/len(Sequence)
