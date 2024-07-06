fae2ce7a985480177af898019eed75564181b03e
tkreusel <tobias.kreusel@t-online.de>
2024-07-06 17:45:58 +0200

    radical cleanup

---
+++ archive/univt2_3_4.py
@@ -0,0 +1,90 @@
+# secondary structure prediction tool, deprecated and discontinued
+helixvalues = {'E':1.59,'A':1.41,'L':1.34,'M':1.3,'Q':1.27,'K':1.23,'R':1.21,'H':1.05,'V':0.9,'I':1.09,'Y':0.74,'C':0.66,'W':1.02,'F':1.16,'T':0.76,'G':0.43,'N':0.76,'P':0.34,'S':0.57,'D':0.99,'U':0.66}
+sheetvalues = {'E':0.52,'A':0.72,'L':1.22,'M':1.14,'Q':0.98,'K':0.69,'R':0.84,'H':0.8,'V':1.87,'I':1.67,'Y':1.45,'C':1.4,'W':1.35,'F':1.33,'T':1.17,'G':0.58,'N':0.48,'P':0.31,'S':0.96,'D':0.39,'U':1.4}
+loopvalues = {'E':1.01,'A':0.82,'L':0.57,'M':0.52,'Q':0.84,'K':1.07,'R':0.9,'H':0.81,'V':0.41,'I':0.47,'Y':0.76,'C':0.54,'W':0.65,'F':0.59,'T':0.9,'G':1.77,'N':1.34,'P':1.32,'S':1.22,'D':1.24,'U':0.54}
+
+def univt2(seq:str, size:float):
+    helic=[]
+    sheet=[]
+    turn=[]
+    for t in range(size,len(seq)-size):
+        frame = [*seq[t - size:t + size+1]]                         #define neighbors of t
+        mnh = sum(list(map(helixvalues.get,frame))) / (2*size+1)
+        mns = sum(list(map(sheetvalues.get,frame))) / (2*size+1)
+        mnt = sum(list(map(loopvalues.get,frame))) / (2*size+1)
+        if mnh > 1.1 and mnh > mns and mnh > mnt:
+            helic.append(t)
+        elif mns > 1 and mns > mnh and mns > mnt:
+            sheet.append(t)
+        elif mnt > 1 and mnt > mnh and mnt > mns:
+            turn.append(t)   
+    counth=0
+    counts=0
+    countt=0
+    for p in range(1,len(helic)-n):
+        if helic[p+2]==helic[p]+2 and helic[p-1]!=helic[p]-1:
+            counth+=1
+    if len(helic) > 2:
+        if helic[2] == helic[0]+2:
+            counth+=1
+    for n in range(1,len(sheet)-n):
+        if sheet[n+2]==sheet[n]+2 and sheet[n-1]!=sheet[n]-1:
+            counts+=1
+    if len(sheet) >= 2:
+        if sheet[2] == sheet[0]+2:
+            counts+=1
+    for s in range(1,len(turn)-n):
+        if turn[s+2]==turn[s]+2 and turn[s-1]!=turn[s]-1:
+            countt+=1
+    if len(turn) > 2:
+        if turn[2] == turn[0]+2:
+            countt+=1
+    
+    return [counth,counts,countt]
+    '''print(f'Helices:{counth}')
+    print(f'Sheets:{counts}')
+    print(f'Turns:{countt} (Nicht getestet)')'''
+     
+     
+#fixed size of 2   
+def univt3(seq:str):
+    helic=[]
+    sheet=[]
+    turn=[]
+    for t in range(2,len(seq)-2):
+        frame = [*seq[t - 2:t + 3]]                         #define neighbors of t
+        mnh = sum(list(map(helixvalues.get,frame))) / 5
+        mns = sum(list(map(sheetvalues.get,frame))) / 5
+        mnt = sum(list(map(loopvalues.get,frame))) / 5
+        if mnh > 1.1 and mnh > mns and mnh > mnt:
+            helic.append(t)
+        elif mns > 1 and mns > mnh and mns > mnt:
+            sheet.append(t)
+        elif mnt > 1 and mnt > mnh and mnt > mns:
+            turn.append(t) 
+    counth=0
+    counts=0
+    countt=0
+    for p in range(1,len(helic)-2):
+        if helic[p+2]==helic[p]+2 and helic[p-1]!=helic[p]-1:
+            counth+=1
+    if len(helic) > 2:
+        if helic[2] == helic[0]+2:
+            counth+=1
+    for n in range(1,len(sheet)-2):
+        if sheet[n+2]==sheet[n]+2 and sheet[n-1]!=sheet[n]-1:
+            counts+=1
+    if len(sheet) >= 2:
+        if sheet[2] == sheet[0]+2:
+            counts+=1
+    for s in range(1,len(turn)-2):
+        if turn[s+2]==turn[s]+2 and turn[s-1]!=turn[s]-1:
+            countt+=1
+    if len(turn) > 2:
+        if turn[2] == turn[0]+2:
+            countt+=1
+    
+    return [counth,counts,countt]
+    '''print(f'Helices:{counth}')
+    print(f'Sheets:{counts}')
+    print(f'Turns:{countt} (Nicht getestet)')'''
---
+++ proteinclass.py
@@ -0,0 +1,57 @@
+class Protein:
+    '''
+    Protein class that takes a sequence and returns a SPARC prediction, optionally with a PDB file for mutational improvement of melting point. Computes features for the sequence and stores them as attributes of the object. Full list of features can be accessed by calling the object's .features attribute.
+    
+    Initial attributes:
+    seq: str, protein sequence
+    PDB: str, name of PDB file in ./data/pdbs directory, optional for SPARC but required for .mutate() method
+    dpath: str, path to directory where temp files will be stored, defaults to ./data/fastas
+    S4path: str, path to directory where S4pred is stored, required for SPARC, defaults to ./data/s4pred
+    
+    Methods:
+    mutate( ): performs mutational improvement of melting point, requires PDB file. Returns mutated melting point, sequence, and difference in melting point and adds them to the object as .mutatedmp, .mutatedseq, and .mutatedmpdiff
+    mutreduce( ): reduces amount of mutations while still maintaining improved melting point. Requires mutate() method to be used first. Returns reduced sequence and melting point and adds them to the object as .mut_seq_reduced and .mut_mp_reduced
+    '''
+    import os
+    import numpy as np
+    def __init__(self, seq:str, PDB = None, dpath = os.path.abspath('./data/fastas'), S4path = os.path.abspath('./data/s4pred')):
+        self.PDB = PDB
+        from SPARC import SPARC
+        self.sequence = seq.upper()
+        result = SPARC(self.sequence, 'placeholder', dpath, S4path)
+        self.SPARC = result[0][0]
+        self.features = result[1]
+        self.features_scaled = result[2]
+        self.features_pca = result[3]
+        for i in self.features.index:
+            setattr(self, i, self.features[i])
+    def __str__(self):
+        return f"Protein with predicted melting point of {str(self.SPARC).strip('[]')} °C"
+    def mutate(self,pqr_output_path='./data/pqrs',iterations = 100, threshhold = 1000):
+        from function_mut import prot_mut
+        output_mut = prot_mut(pdb_path = './data/pdbs', pdb_file=self.PDB,pqr_output_path=pqr_output_path,iterations = iterations, threshhold = threshhold)
+        self.mutatedmp = output_mut[0][1][0][0]
+        self.mutatedseq = output_mut[1][0]
+        self.mutatedmpdiff = self.mutatedmp - self.SPARC
+        mutationlist = []
+        for n in range(len(self.mutatedseq)):
+            if self.mutatedseq[n] != self.sequence[n]:
+                mutationlist.append(f'{self.sequence[n]}{n+1}{self.mutatedseq[n]}')
+        self.mutationlist = mutationlist
+        return [self.mutatedmp, self.mutatedseq, self.mutatedmpdiff, self.mutationlist]
+    def mutreduce(self,name = 'proteinxyz'):
+        from function_mut import mutation_decreaser
+        outputdecreaser = mutation_decreaser(mut_temp = self.mutatedmp, wt_temp = self.SPARC, wt_protein = self.sequence, mut_protein = self.mutatedseq, name = name)
+        self.mut_mp_reduced = outputdecreaser[2]
+        self.mut_seq_reduced = outputdecreaser[0]
+        mutationlist_reduced = []
+        for n in range(len(self.mut_seq_reduced)):
+            if self.mut_seq_reduced[n] != self.sequence[n]:
+                mutationlist_reduced.append(f'{self.sequence[n]}{n+1}{self.mut_seq_reduced[n]}')
+        self.mutationlist_reduced = mutationlist_reduced
+        removed_mutations = []
+        for n in range(len(self.mutatedseq)):
+            if self.mutatedseq[n] != self.mut_seq_reduced[n]:
+                removed_mutations.append(f'{self.mutatedseq[n]}{n+1}')
+        self.removed_mutations = removed_mutations
+        return [self.mut_mp_reduced, self.mut_seq_reduced, self.mutationlist_reduced]
